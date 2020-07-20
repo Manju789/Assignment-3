b1bc14bc343a66a75cd2a6682ce7f5856ee61e6e
Manju789 <68197488+Manju789@users.noreply.github.com>
2020-07-20 12:22:55 +0530

    Add files via upload

---
+++ GiftCard.java
@@ -0,0 +1,44 @@
+package Demo;
+
+public class GiftCard {
+	private long cardNo;
+	private double balance;
+	private boolean active;
+	public GiftCard(long cardNo, double balance, boolean active) {
+		super();
+		this.cardNo = cardNo;
+		this.balance = balance;
+		this.active = active;
+	}
+	public GiftCard() {
+		//super();
+		this.balance=0;
+	}
+	public void rechargeCard(double amount) {
+		if(active)
+			this.balance=this.getBalance()+amount;
+	}
+	public void swipeCard(double amount) {
+		if(this.active==true && this.balance>amount)
+			this.balance=this.getBalance()-amount;
+	}
+	public long getCardNo() {
+		return cardNo;
+	}
+	public void setCardNo(long cardNo) {
+		this.cardNo = cardNo;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	/*public void setBalance(double balance) {
+		this.balance = balance;
+	}*/
+	public boolean isActive() {
+		return active;
+	}
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+	
+}
